d627efed4770277c292562de7e8f06c7ee26dcbb
Pacholleck <dustin.pacholleck@gmail.com>
2022-04-25 21:48:52 +0200

    remove up and downvotes

--- scrapy/urbandict/urbandict/spiders/words.py
+++ scrapy/urbandict/urbandict/spiders/words.py
@@ -6,8 +6,8 @@ import scrapy
 class Words(scrapy.Item):
     name        = scrapy.Field()
     author      = scrapy.Field()
-    upvotes     = scrapy.Field()
-    downvotes   = scrapy.Field() 
+#    upvotes     = scrapy.Field()
+#    downvotes   = scrapy.Field() 
     date        = scrapy.Field()
     # meaning     = scrapy.Field()
 
@@ -26,19 +26,16 @@ class LinksSpider(scrapy.Spider):
 
         name_xpath          = '//h1/a[re:test(@href, "/define.*")]/text()'
         author_xpath         = '//a[re:test(@href, "/author.*")]/text()'
-        upvotes_xpath          = '//div[@data-direction="up"]/span'
-        downvotes_xpath      = '//div[@data-direction="down"]/span/text()'
+        #upvotes_xpath          = '//div[@data-direction="up"]/span'
+        #downvotes_xpath      = '//div[@data-direction="down"]/span/text()'
         date_xpath         = '//a[re:test(@href, "/author.*")]/following-sibling::text()'
         # meaning_xpath         = '//div[@aria-label="Piętro"]/div[2]/div/text()'    
        
         p['name']       = response.xpath(name_xpath).get()
         p['author']     = response.xpath(author_xpath).get()
-        p['upvotes']    = response.xpath(upvotes_xpath).get()
-        p['downvotes']  = response.xpath(downvotes_xpath).getall()
+        #p['upvotes']    = response.xpath(upvotes_xpath).get()
+        #p['downvotes']  = response.xpath(downvotes_xpath).getall()
         p['date']       = response.xpath(date_xpath).get()
-        # p['level']      = response.xpath(level_xpath).getall()
-        # p['condition']  = response.xpath(condition_xpath).getall()
-        # p['decription'] = response.xpath(description_xpath).getall()
 
         
 
