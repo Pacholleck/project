f8ae9b453a003ac90c44d5b75aa72e08241759bc
MichalStryjek <98279476+MichalStryjek@users.noreply.github.com>
2022-05-09 14:00:21 +0200

    add geckodriver

--- selenium/urban_seleniu.py
+++ selenium/urban_seleniu.py
@@ -1,4 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 import time
-rt
+
+# Init:
+gecko_path = '/geckodriver/geckodriver.exe'
+ser = Service(gecko_path)
+options = webdriver.firefox.options.Options()
+options.headless = False
+driver = webdriver.Firefox(options = options, service=ser)
