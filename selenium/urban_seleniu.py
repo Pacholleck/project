19ef59e8106437774a9cdf6afa038f801541643d
MichalStryjek <98279476+MichalStryjek@users.noreply.github.com>
2022-05-10 20:58:22 +0200

    Finished

--- selenium/urban_seleniu.py
+++ selenium/urban_seleniu.py
@@ -1,4 +1,4 @@
-limiter=False
+limiter = False
 global counter
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -10,63 +10,84 @@ import pandas as pd
 
 ### settings ##########
 
-custom_limit=5
+custom_limit = 400
 
-word_pages=1
+word_pages = 5
 
-wait_time=2
-
-#######################
+wait_time = 2
 
 
-#get list of the pages to scrap
+# get list of the pages to scrap
 def get_wordlinks(limit=word_pages):
-    words=[]
-    for i in range(1,limit+1):
-        url=f'http://urbandictionary.com?page={i}'
+    words = []
+    for i in range(1, limit + 1):
+        url = f'http://urbandictionary.com?page={i}'
         driver.get(url)
         time.sleep(wait_time)
-        #try:
-        xpath='//h1/a[contains(@href,"define.php?term=")]'
-        words_temp=([my_elem.get_attribute("href") for my_elem in driver.find_elements(By.XPATH,xpath)])
+        # try:
+        xpath = '//h1/a[contains(@href,"define.php?term=")]'
+        words_temp = ([my_elem.get_attribute("href") for my_elem in driver.find_elements(By.XPATH, xpath)])
         words.extend(words_temp)
-        #except:
-            #words=words.append([my_elem.get_attribute("href") for my_elem in driver.find_elements(By.XPATH,"//h1/a")])
-    return(words)
+        # except:
+        # words=words.append([my_elem.get_attribute("href") for my_elem in driver.find_elements(By.XPATH,"//h1/a")])
+    return (words)
 
 
 def getpagecontent(url):
+    #dict = {'author': [], 'date': [], 'word': []}
+    df=pd.DataFrame({'word': [],'author': [],'date': []})
     driver.get(url)
     time.sleep(wait_time)
-    auth_xpath="//div[contains(@class,'contributor')]/a[contains(@href,'.php?author=')]"
-    authors=driver.find_elements(By.XPATH,auth_xpath)
-    dict={'name':[],'date':[],'word':[]}
+    # auth_xpath="//div[contains(@class,'contributor')]/a[contains(@href,'.php?author=')]"
+    #auth_xpath = "//div[contains(@class,'contributor')]"
+    div_xpath='//div[contains(@class,"p-5 md:p-8")]'
+    authors = driver.find_elements(By.XPATH, div_xpath)
     for div in authors:
-        author=div.find_element(By.XPATH,"//a[contains(@href,'.php?author=')]").get_attribute('text')
-        print(author)
-        dict= {'name':author}
-        print(dict)
-    return(dict)
+        d=div.find_element(By.XPATH,".//div[contains(@class,'contributor')]")
+        d=d.get_attribute('innerHTML').split('</a>')[1].strip()
+        a=div.find_element(By.XPATH,".//div/a[contains(@href,'.php?author=')]").get_attribute('text')
+        h = div.find_element(By.XPATH, "./div/h1/a[contains(@href,'define')]").get_attribute('text')
+        dict={'word':h,'author':a,'date': d}
+        df=df.append(dict,ignore_index=True)
+    return (df)
 
-def merge_frame_and_dictionary(dic,pd):
+
+def merge_frame_and_dictionary(dic, pd):
     print(dic)
-    return("aaaaaaaaaa")
-
-########limiter function#############
+    return ("aaaaaaaaaa")
 
 
-if limiter==True:
-    counter_limit=100
+######## limiter function #############
+
+
+if limiter == True:
+    counter_limit = 100
 else:
-    counter_limit=custom_limit
+    counter_limit = custom_limit
 
+############ setup driver #############
 try:
-    # setup driver
+
     dname = pth.dirname(__file__)
-    lpath = '/chromedriver101/chromedriver.exe'
+    lpath = '/geckodriver/geckodriver.exe'
     # Init:
     gecko_path = dname + lpath
     print(gecko_path)
+    ser = Service(gecko_path)
+    options = webdriver.firefox.options.Options()
+    options.headless = False
+    driver = webdriver.Firefox(options=options, service=ser)
+
+    url = 'http://urbandictionary.com'
+
+
+except:
+
+    # setup driver in case chrome is in version 102
+    dname = pth.dirname(__file__)
+    lpath = '/chromedriver/chromedriver.exe'
+    # Init:
+    gecko_path = dname + lpath
     ser = Service(gecko_path)
     options = webdriver.chrome.options.Options()
     options.headless = False
@@ -74,57 +95,51 @@ try:
 
     url = 'http://urbandictionary.com'
 
-except:
+#### starting value of page counter ############
+counter = 1
 
-    #setup driver in case chrome is in version 102
-    dname=pth.dirname(__file__)
-    lpath='/chromedriver/chromedriver.exe'
-    # Init:
-    gecko_path = dname + lpath
-    print(gecko_path)
-    ser = Service(gecko_path)
-    options = webdriver.chrome.options.Options()
-    options.headless = False
-    driver = webdriver.Chrome(options = options, service=ser)
+###### list of links to words from main page ##########
+wordlist = get_wordlinks()
 
-    url = 'http://urbandictionary.com'
+##### create output dataframe #########
+output_frame = pd.DataFrame({'word': [],'author': [],'date': []})
 
-
-
-
-counter=0
-
-wordlist=get_wordlinks()
-
-
-print(len(wordlist))
-output_frame=pd.DataFrame({'name':[],'date':[],'word':[]})
-
+#### go through link of every word from the main page ########
 for link in wordlist:
-    driver.get(link)
-    xpath = '//a[text()="Last »"]'
-    try:
-        my_elem = driver.find_element(By.XPATH, xpath)
-        last_number = my_elem.get_attribute("href")
-    except:
-        last_number=None
 
-    if last_number is None:
-        getpagecontent(link)
+    ####### check whether the limit of the pages is reached ########
+    if counter <= counter_limit:
+        driver.get(link)
+
+        ####### XPath to search for the "last" button ######
+        xpath = '//a[text()="Last »"]'
+
+        ####### get number of the last page, or return None if there isn't one ############
+        try:
+            my_elem = driver.find_element(By.XPATH, xpath)
+            last_number = my_elem.get_attribute("href")
+        except:
+            last_number = None
+
+        ############if there is no last button then there is only one page. scrap data from the page and increase the counter for page limit##############
+        if last_number is None:
+            fdf=getpagecontent(link)
+            output_frame=pd.concat([output_frame,fdf])
+            counter = counter + 1
+
+        ########## if there is a last page number find it and loop through all pages until the last increasing counter for each page scraped###########
+        else:
+            pages = re.findall('(\d+)', last_number)
+            for s in range(1, int(pages[0]) + 1):
+                url2 = f'{link}&page={s}'
+                fdf=getpagecontent(url2)
+                output_frame=pd.concat([output_frame,fdf])
+    ############ upon reaching page limit return information and break loop############
     else:
-        pages = re.findall('(\d+)', last_number)
-        for s in range(1, int(pages[0]) + 1):
-            url2= f'{link}&page={s}'
-            if counter<counter_limit:
+        print("page limite reached")
+        break
 
-                if s == 1:
-                    getpagecontent(link)
-                    counter=counter+1
-                else:
-                    getpagecontent(url2)
-                    counter=counter+1
-            else:
-                print(f'Exit forced due to page limit')
-                break
 
+print(len(output_frame))
+######## close connection ##########
 driver.close()
